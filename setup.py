52a97c26764e293d79ceaad304bbce891da9f432
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2015-10-01 17:12:20 +0200

    commented out non working build stuff

--- setup.py
+++ setup.py
@@ -6,77 +6,77 @@ from setuptools import setup, find_packages
 from subprocess import call
 from multiprocessing import cpu_count
 
-BASEPATH = os.path.dirname(os.path.abspath(__file__))
-SECP_PATH = os.path.join(BASEPATH, 'bitcoinSecp256k1')
+# BASEPATH = os.path.dirname(os.path.abspath(__file__))
+# SECP_PATH = os.path.join(BASEPATH, 'bitcoinSecp256k1')
 
-class BitcoinSecpBuild(build):
-    def run(self):
-        build.run(self)
+# class BitcoinSecpBuild(build):
+    # def run(self):
+        # build.run(self)
 
-        #build bitcoinSecp256k1
-        build_path = os.path.abspath(self.build_temp)
+        # #build bitcoinSecp256k1
+        # build_path = os.path.abspath(self.build_temp)
 
-        cmd = [
-                './autogen.sh',
-                './configure --enable-shared --enable-module-recovery',
-                'make',
-                'OUT=' + build_path,
-                'V=' + str(self.verbose),
-            ]
+        # cmd = [
+                # './autogen.sh',
+                # './configure --enable-shared --enable-module-recovery',
+                # 'make',
+                # 'OUT=' + build_path,
+                # 'V=' + str(self.verbose),
+            # ]
 
-        try:
-            cmd.append('-j%d' % cpu_count())
-        except NotImplementedError:
-            print 'Unable to determine number of CPUs. Using single threaded make.'
+        # try:
+            # cmd.append('-j%d' % cpu_count())
+        # except NotImplementedError:
+            # print 'Unable to determine number of CPUs. Using single threaded make.'
 
-        options = [
-            'DEBUG=n',
-            'ENABLE_SDL=n',
-        ]
-        cmd.extend(options)
+        # options = [
+            # 'DEBUG=n',
+            # 'ENABLE_SDL=n',
+        # ]
+        # cmd.extend(options)
 
-        targets = ['python']
-        cmd.extend(targets)
+        # targets = ['python']
+        # cmd.extend(targets)
 
-        if platform == 'darwin':
-            target_path = 'OSX64_PYTHON'
-        else:
-            target_path = 'UNIX_PYTHON'
+        # if platform == 'darwin':
+            # target_path = 'OSX64_PYTHON'
+        # else:
+            # target_path = 'UNIX_PYTHON'
 
-        # target_files = [os.path.join(build_path, target_path, '.lib', 'libsecp256k1.so')]
+        # # target_files = [os.path.join(build_path, target_path, '.lib', 'libsecp256k1.so')]
 
-        def compile():
-            call(cmd, cwd=SECP_PATH)
+        # def compile():
+            # call(cmd, cwd=SECP_PATH)
 
-        self.execute(compile, [], 'Compiling bitcoin secp256k1')
+        # self.execute(compile, [], 'Compiling bitcoin secp256k1')
 
-        # copy resulting tool to library build folder
-        self.mkpath(self.build_lib)
+        # # copy resulting tool to library build folder
+        # self.mkpath(self.build_lib)
 
-        # if not self.dry_run:
-            # for target in target_files:
-                # self.copy_file(target, self.build_lib)
-
-
-class BitcoinSecpInstall(install):
-    def initialize_options(self):
-        install.initialize_options(self)
-        self.build_scripts = None
-
-    def finalize_options(self):
-        install.finalize_options(self)
-        self.set_undefined_options('build', ('build_scripts', 'build_scripts'))
-
-    def run(self):
-        # run original install code
-        install.run(self)
-
-        # install XCSoar executables
-        self.copy_tree(self.build_lib, self.install_lib)
+        # # if not self.dry_run:
+            # # for target in target_files:
+                # # self.copy_file(target, self.build_lib)
 
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+# class BitcoinSecpInstall(install):
+    # def initialize_options(self):
+        # install.initialize_options(self)
+        # self.build_scripts = None
+
+    # def finalize_options(self):
+        # install.finalize_options(self)
+        # self.set_undefined_options('build', ('build_scripts', 'build_scripts'))
+
+    # def run(self):
+        # # run original install code
+        # install.run(self)
+
+        # # install XCSoar executables
+        # self.copy_tree(self.build_lib, self.install_lib)
+
+
+# def read(fname):
+    # return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 
 setup(
@@ -99,8 +99,8 @@ setup(
         "Programming Language :: Python :: Implementation :: PyPy"
     ],
     zip_safe=False,
-    cmdclass={
-        'build': BitcoinSecpBuild,
-        'install': BitcoinSecpInstall,
-    }
+    # cmdclass={
+        # 'build': BitcoinSecpBuild,
+        # 'install': BitcoinSecpInstall,
+    # }
 )
