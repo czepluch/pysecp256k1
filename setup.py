a080b22fb316cc75df241625eca542aff6d56d34
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2015-10-06 17:14:50 +0200

    removed .so file from setup

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ class BitcoinSecpBuild(build):
 
 setup(
     name="pysecp256k1",
-    version='0.0.3',
+    version='0.0.4',
     description="secp256k1 wrapped with cffi to use with python",
     author="Jacob Stenum Czepluch",
     author_email="j.czepluch@gmail.com",
     url="https://github.com/czepluch/pysecp256k1",
     license="MIT",
     packages=find_packages(exclude=["_cffi_build", "_cffi_build/*"]),
-    package_data={'': ['libsecp256k1.so']},
+    # package_data={'': ['libsecp256k1.so']},
     install_requires=["cffi>=1.2.1"],
     setup_requires=["cffi>=1.2.1"],
     cffi_modules=["_cffi_build/pysecp256k1_build.py:ffi"],
