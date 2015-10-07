d106b9ec7af7a1d4fdb639ae5027b38bcdaac411
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2015-10-07 17:15:30 +0200

    mac osx compatibility

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     url="https://github.com/czepluch/pysecp256k1",
     license="MIT",
     packages=find_packages(exclude=["_cffi_build", "_cffi_build/*"]),
-    package_data={'': ['libsecp256k1.so']},
+    package_data={'': ['libsecp256k1.*']},
     install_requires=["cffi>=1.2.1", "requests>=2.8.0"],
     setup_requires=["cffi>=1.2.1", "requests>=2.8.0"],
     cffi_modules=["_cffi_build/secp256k1_build.py:ffi"],
