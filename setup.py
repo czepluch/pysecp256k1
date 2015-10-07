d8a5172ae3644967a830f469f07f90e0334617a9
Jacob Stenum Czepluch <j.czepluch@gmail.com>
2015-10-07 17:29:40 +0200

    fixing setup.py issue hacks

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 # from distutils.command.build import build
 from setuptools import setup, find_packages
 from subprocess import call
+import requests
+
+setup(install_requires=["requests>=2.8.0"])
+
+#  Fetching the bitcoin_secp256k1 tarball from github and running build.sh if success
+bitcoin_secp256k1 = "https://github.com/bitcoin/secp256k1/tarball/master"
+print("\nDownloading tarball from " + bitcoin_secp256k1 + "...")
+r = requests.get(bitcoin_secp256k1)
+if r.status_code == 200:
+    print("Successfully fetched the tarball :) ")
+    open('secp256k1.tar.gz', 'wb').write(r.content)
+    call("./build.sh", shell=True)
+else:
+    print("something went wrong while downloading " + bitcoin_secp256k1)
+    print(r.status_code)
 
 
 setup(
@@ -13,8 +28,8 @@ setup(
     license="MIT",
     packages=find_packages(exclude=["_cffi_build", "_cffi_build/*"]),
     package_data={'': ['libsecp256k1.*']},
-    install_requires=["cffi>=1.2.1", "requests>=2.8.0"],
-    setup_requires=["cffi>=1.2.1", "requests>=2.8.0"],
+    install_requires=["cffi>=1.2.1"],
+    setup_requires=["cffi>=1.2.1"],
     cffi_modules=["_cffi_build/secp256k1_build.py:ffi"],
     classifiers=[
         "Programming Language :: Python :: 2",
@@ -23,17 +38,3 @@ setup(
     ],
     zip_safe=False,
 )
-
-
-#  Fetching the bitcoin_secp256k1 tarball from github and running build.sh if success
-import requests
-bitcoin_secp256k1 = "https://github.com/bitcoin/secp256k1/tarball/master"
-print("\nDownloading tarball from " + bitcoin_secp256k1 + "...")
-r = requests.get(bitcoin_secp256k1)
-if r.status_code == 200:
-    print("Successfully fetched the tarball :) ")
-    open('secp256k1.tar.gz', 'wb').write(r.content)
-    call("./build.sh", shell=True)
-else:
-    print("something went wrong while downloading " + bitcoin_secp256k1)
-    print(r.status_code)
