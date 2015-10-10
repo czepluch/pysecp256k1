193bc186e2c23faaee8c64e17288661f0b2a63a3
Heiko Heiko <heiko@heiko.org>
2015-10-10 15:38:28 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -32,14 +32,13 @@ else:
 
 setup(
     name="c_secp256k1",
-    version='0.0.2',
+    version='0.0.3',
     description="secp256k1 wrapped with cffi to use with python",
     author="Jacob Stenum Czepluch",
     author_email="j.czepluch@gmail.com",
     url="https://github.com/czepluch/pysecp256k1",
     license="MIT",
     packages=find_packages(exclude=["_cffi_build", "_cffi_build/*"]),
-    package_data={'': ['libsecp256k1.*']},
     install_requires=["cffi>=1.2.1", "bitcoin>=1.1.36"],
     setup_requires=["cffi>=1.2.1"],
     cffi_modules=["_cffi_build/c_secp256k1_build.py:ffi"],
